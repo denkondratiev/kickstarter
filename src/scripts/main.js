5094828bff02a21e51f81e81c38bfd1daf715942
denkondratiev <denys.kondratiev.job@gmail.com>
2020-05-21 00:42:12 +0300

    add mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,70 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 'use strict';
 
-$('.form').submit(function() {
-  $('.form__success').addClass('form__success--active').hide().fadeIn();
-
-  setTimeout(function() {
-    $('.form__success').removeClass('form__success--active').fadeOut();
-    $('.form').trigger('reset');
-  }, 2000);
+$('.burger').click(function() {
+  $(this).toggleClass('burger--active');
+  $('.nav__mobile').toggleClass('nav__mobile--active');
 
   return false;
+});
+
+$('.form').submit(function() {
+  $('.success').addClass('success--active').hide().fadeIn();
+
+  return false;
+});
+
+$('.success').click(function() {
+  $('.success').fadeOut('slow');
+  $('.form').trigger('reset');
+
+  return false;
+});
+
+$('.button--success').click(function() {
+  $('html, body').stop().animate({ scrollTop: 0 }, 'slow', 'swing');
+});
+
+$('.circle').click(function() {
+  $(this).toggleClass('circle--active');
+  $(this).parent().toggleClass('square--active');
+  $(this).parent().toggleClass('square__text--active');
+
+  return false;
+});
+
+$('.services').slick({
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  speed: 300,
+  slidesToShow: 1,
+  prevArrow: $('.prev'),
+  nextArrow: $('.next'),
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 686,
+      settings: 'unslick',
+    },
+  ],
+});
+
+$('.features__gallery').slick({
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  speed: 300,
+  slidesToShow: 1,
+  prevArrow: $('.features__prev'),
+  nextArrow: $('.features__next'),
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 686,
+      settings: 'unslick',
+    },
+  ],
 });
