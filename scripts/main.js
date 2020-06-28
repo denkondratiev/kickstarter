4bef8cdb9dd5c2218e8eadea572e812731d3d37c
denkondratiev <denys.kondratiev.job@gmail.com>
2020-06-29 00:02:05 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,65 @@
-/* eslint-disable camelcase */
-/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 'use strict';
 
-$('.burger').click(function() {
-  $(this).toggleClass('burger--active');
-  $('.nav__mobile').toggleClass('nav__mobile--active');
+// Burger toggler
+const burger = document.querySelector('.burger');
+const navMobile = document.querySelector('.nav__mobile');
+
+function toggleBurger() {
+  burger.classList.toggle('burger--active');
+  navMobile.classList.toggle('nav__mobile--active');
+}
+
+burger.addEventListener('click', toggleBurger);
+
+// Technology slider
+const techPhoto = document.querySelector('.technology__photo');
+const prev = document.querySelector('.prev--technology');
+const next = document.querySelector('.next--technology');
+const gap = 300;
+
+let position = -80;
+
+function slideLeft(e) {
+  position -= gap;
+
+  if (Math.abs(position) > 1000) {
+    position = -80;
+  };
+
+  techPhoto.style.transform = `translateX(${position}px)`;
+};
+
+function slideRight(e) {
+  position += gap;
+
+  if (position > 0) {
+    position = -80;
+  };
+
+  techPhoto.style.transform = `translateX(${position}px)`;
+};
+
+next.addEventListener('click', slideLeft);
+prev.addEventListener('click', slideRight);
+// End technology slider
+
+// Technology circles toggler
+const circle = document.querySelectorAll('.circle');
+
+function toggleTechnology(e) {
+  e.target.classList.toggle('circle--active');
+  e.target.parentElement.classList.toggle('square--active');
+  e.target.nextElementSibling.classList.toggle('square__text--active');
 
   return false;
-});
+}
 
+for (const item of circle.values()) {
+  item.addEventListener('click', toggleTechnology);
+}
+
+// Form submit
 $('.form').submit(function() {
   $('.success').addClass('success--active').hide().fadeIn();
 
@@ -17,7 +67,7 @@ $('.form').submit(function() {
 });
 
 $('.success').click(function() {
-  $('.success').fadeOut('slow');
+  $('.success').removeClass('success--active').fadeOut('slow');
   $('.form').trigger('reset');
 
   return false;
@@ -27,14 +77,7 @@ $('.button--success').click(function() {
   $('html, body').stop().animate({ scrollTop: 0 }, 'slow', 'swing');
 });
 
-$('.circle').click(function() {
-  $(this).toggleClass('circle--active');
-  $(this).parent().toggleClass('square--active');
-  $(this).parent().toggleClass('square__text--active');
-
-  return false;
-});
-
+// Slick slider
 $('.services').slick({
   dots: false,
   infinite: true,
